4820ce07800cc8d28f813ca644c28305622c21a2
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-03-10 21:19:32 +0530

    removed changes
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/app/api/coffeechat/route.ts
+++ src/app/api/coffeechat/route.ts
@@ -4,8 +4,6 @@ import { langchainInvoke } from './langchain';
 import { setScopes } from "monocle2ai"
 
 export const POST = async (request: Request) => {
-  console.log("MONOCLE_EXPORTER")
-  console.log(process.env.MONOCLE_EXPORTER)
   const sessionId = request.headers.get('X-Session-Id');
   logger.info(`Processing request for session: ${sessionId}`);
   process.env["MONOCLE_S3_KEY_PREFIX_CURRENT"] = sessionId ? sessionId : ""
