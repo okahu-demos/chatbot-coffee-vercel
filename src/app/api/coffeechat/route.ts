d83dc77529bcb6384068a24f04c82e12b0e4f0b7
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-03-10 21:15:38 +0530

    added new monocle with scope
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/app/api/coffeechat/route.ts
+++ src/app/api/coffeechat/route.ts
@@ -1,18 +1,27 @@
 import { logger } from '@/libs/Logger';
 import { NextResponse } from 'next/server';
 import { langchainInvoke } from './langchain';
+import { setScopes } from "monocle2ai"
 
 export const POST = async (request: Request) => {
   console.log("MONOCLE_EXPORTER")
   console.log(process.env.MONOCLE_EXPORTER)
   const sessionId = request.headers.get('X-Session-Id');
   logger.info(`Processing request for session: ${sessionId}`);
-  process.env["MONOCLE_S3_KEY_PREFIX_CURRENT"] = sessionId ? sessionId + "__" : ""
+  process.env["MONOCLE_S3_KEY_PREFIX_CURRENT"] = sessionId ? sessionId : ""
 
   const json = await request.json()
   console.log("Request:", json.message);
 
-  const llmResponse = await langchainInvoke(json.message);
+  const llmResponse = await setScopes(
+    {
+      "sessionId": sessionId
+    },
+    () => {
+      return langchainInvoke(json.message);
+    })
+
+  // const llmResponse = await langchainInvoke(json.message);
 
   console.log("Response from llm:", llmResponse);
 
